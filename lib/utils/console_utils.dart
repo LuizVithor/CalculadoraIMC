360e2cec30f64ba3c90d770ba4fb621ffbe0eb68
Luiz Vithor <luizmonodarius@gmail.com>
2023-10-16 01:18:43 -0300

    Desafio finalizado

--- lib/utils/console_utils.dart
+++ lib/utils/console_utils.dart
@@ -1,12 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 
-/// Classe utilitária para tratar inputs
 class ConsoleUtils {
-  /// # Ler string digitada no console
-  ///
-  /// * Exemplo sem mensagem: `ConsoleUtils.lerString();`
-  /// * Exemplo com mensagem: `ConsoleUtils.lerString(mensagem: 'Exemplo');`
+
   static String lerString({String mensagem = ''}) {
     if (mensagem.trim() != '') {
       print(mensagem);
@@ -16,16 +12,11 @@ class ConsoleUtils {
     return linha ?? '';
   }
 
-  /// # Ler double digitado no console
-  ///
-  /// * Exemplo sem mensagem: `ConsoleUtils.lerDouble();`
-  /// * Exemplo com mensagem: `ConsoleUtils.lerDouble(mensagem: 'Exemplo');`
   static double lerDouble({String mensagem = ''}) {
     do {
       try {
         var linha = lerString(mensagem: mensagem);
 
-        // Tratar número no formato 0.000,00
         if (linha.contains(',')) {
           linha = linha.replaceAll('.', '');
           linha = linha.replaceAll(',', '.');
